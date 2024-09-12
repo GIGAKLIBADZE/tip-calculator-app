ea2af745bbe6a16813f319b68073e7072df062aa
Giga Klibadze <giga.klibadze.1@btu.edu.ge>
2024-09-13 00:36:15 +0400

    finished project

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Tip calculator app solution
+
+## Overview
+
+This is a solution to the Tip calculator app challenge on Frontend Mentor. Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- Receive an error message when the form is submitted if:
+  - The people amount field is incorrectly filled in (is equal to 0)"
+
+### Links
+
+Solution URL: [https://github.com/GIGAKLIBADZE/tip-calculator-app]
+Live Site URL: [https://gigaklibadze.github.io/tip-calculator-app/]
+
+## My process
+
+I used mobile first workflow while building this project. Then I added/modified styles for the desktop version. Finally, I added some javascript code block for the desktop version, I used DOM for these things.
+
+### Built with
+
+Semantic HTML5 markup
+CSS custom properties
+Flexbox
+Mobile-first workflow
+JavaScript
+DOM
+
+### What I learned
+
+After created many HTML/CSS projects and in one of them I used a tiny little JavaScript, In this one I learned how to use use DOM also. I got familiar with HTML input tag specifications and some CSS features which were brand new for me. And the most important is how my knowledge increased about DOM after building this project.
+
+### Continued development
+
+Now, after building some markup projects, I am going to deeply learn JavaScript and DOM. Then I will continue constructing more complex projects by using my new knowledge.
+
+### Useful resources
+
+[https://algoUni.ge/] - Mentors from this course helped me to get more familiar with integrate JavaScrip in my project. Here you can find mentorship service, which helps me a lot.
+[https://www.w3schools.com/] - This popular site is very useful to find many features that you need while building projects.
+[https://chat.openai.com/] - Also the popular tool, created by AI helped me to find and understand some topics about CSS.
+
+Author
+Website - [https://github.com/GIGAKLIBADZE]
---
+++ app.js
@@ -0,0 +1,99 @@
+const billInput = document.getElementById("bill");
+const percentageButtons = [...document.getElementsByClassName("percentage")];
+const peopleInput = document.getElementById("people-amount");
+const resultTip = document.getElementById("final-tip");
+const resultTotal = document.getElementById("final-total");
+const resetAll = document.getElementById("reset");
+const customInput = [...document.getElementsByClassName("custom")];
+const errorMessage = document.getElementById("error-message");
+
+let billValue;
+let percentageValue;
+let peopleValue;
+
+billInput.addEventListener("input", (event) => {
+  billValue = parseFloat(event.target.value);
+
+  if (billValue > 0) {
+    billInput.style.color = "#00474b";
+    billInput.style.fontSize = "24px";
+    billInput.style.fontWeight = "bold";
+  }
+});
+
+percentageButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    percentageValue = parseInt(event.target.textContent);
+
+    percentageButtons.forEach((button) => (button.style.backgroundColor = ""));
+
+    button.style.backgroundColor = "#26c2ae";
+    calculation();
+  });
+});
+
+customInput.forEach((input) => {
+  input.addEventListener("input", (event) => {
+    percentageValue = parseInt(event.target.value);
+
+    customInput.forEach((input) => (input.style.backgroundColor = ""));
+
+    if (percentageValue > 0) {
+      event.target.style.color = "#00474b";
+      event.target.style.fontSize = "24px";
+      event.target.style.fontWeight = "bold";
+    }
+
+    calculation();
+  });
+});
+
+peopleInput.addEventListener("input", (event) => {
+  peopleValue = event.target.value;
+
+  if (peopleValue > 0) {
+    peopleInput.style.color = "#00474b";
+    peopleInput.style.fontSize = "24px";
+    peopleInput.style.fontWeight = "bold";
+  }
+
+  if (parseInt(peopleValue) === 0) {
+    peopleInput.style.outlineColor = "#e17052";
+    errorMessage.textContent = "Can't be zero";
+  }
+  calculation();
+});
+
+function calculation() {
+  const billAmount = parseFloat(billValue);
+  const peopleAmount = parseFloat(peopleValue);
+  const resultTipValue = (billAmount * (percentageValue / 100)) / peopleAmount;
+  const resultTotalValue = billAmount / peopleAmount + resultTipValue;
+
+  if (peopleValue !== 0 && peopleValue) {
+    resultTip.textContent = `$${resultTipValue.toFixed(2)}`;
+    resultTotal.textContent = `$${resultTotalValue.toFixed(2)}`;
+  }
+}
+
+resetAll.addEventListener("click", (event) => {
+  billInput.value = "";
+  peopleInput.value = "";
+  billValue = 0;
+  percentageValue = 0;
+  peopleValue = 0;
+  resultTip.textContent = `$0.00`;
+  resultTotal.textContent = `$0.00`;
+
+  peopleInput.style.outlineColor = "#26c2ae";
+
+  percentageButtons.forEach((button) => {
+    button.style.backgroundColor = "#00474b";
+  });
+
+  customInput.forEach((input) => {
+    input.value = "";
+    input.style.backgroundColor = "#f3f9fa";
+    errorMessage.textContent = "";
+  });
+});
